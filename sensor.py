6d6a54dcf978cc81f829dd3ad5e2a702a29907e0
Christopher Mooney <chris@dod.net>
2014-01-04 13:51:52 -0500

    Developed remotely, pushing for testing

--- sensor.py
+++ sensor.py
@@ -1,26 +1,30 @@
 #!/usr/bin/env python
 
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+class GDIAlarmSensor:
+  """
+  An GDIAlarmSensor class
+  
+  Usage: subclass the GDIAlarmSensor class and call run() when ready
+  """
+  def __init__(self, bouncetime, mode=GPIO.BCM):
+    self.mode = mode
+    self.bouncetime = bouncetime
+    GPIO.setmode(mode)
+    GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    def sprinkler_alarm(channel):
+      print "sprinkler alarm detected on 17"
+    def fire_alarm(channel):
+      print "fire alarm detected on 23"
+    GPIO.add_event_detect(17, GPIO.FALLING, callback=sprinkler_alarm, bouncetime=bouncetime)
+    GPIO.add_event_detect(23, GPIO.FALLING, callback=fire_alarm,      bouncetime=bouncetime)
 
-def sprinkler_alarm(channel):
-  print "sprinkler alarm detected on 17"
+  def __del__(self):
+    GPIO.cleanup()
 
-def fire_alarm(channel):
-  print "fire alarm detected on 23"
-
-GPIO.add_event_detect(17, GPIO.FALLING, callback=sprinkler_alarm, bouncetime=300)
-GPIO.add_event_detect(23, GPIO.FALLING, callback=fire_alarm,      bouncetime=300)
-
-try:
-  GPIO.wait_for_edge(24, GPIO.RISING)
-  print ""
-
-except KeyboardInterrupt:
-  GPIO.cleanup()
-
-GPIO.cleanup()
+  def run(self):
+    while True:
+      time.sleep(1)
